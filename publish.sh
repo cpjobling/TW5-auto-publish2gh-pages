ca866fb76dc1b3f1217a17315e7380e10ea9c228
Chris Jobling <cpjobling@gmail.com>
2018-01-08 00:18:38 +0000

    added creation of CNAME to output

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@
 
 # go to the output directory and create a new git repo
 cd ./*wiki/output || exit 1 # abort script if folder does not exists
+echo "journal.cpjobling.net" > ./*wiki/output/CNAME
 git init
 
 # inside this git repo we'll pretend to be a new user
