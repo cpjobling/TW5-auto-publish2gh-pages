0e68f1a0cf8ec5982c4ab36d973882a13078b67a
Chris Jobling <cpjobling@gmail.com>
2018-05-27 12:52:12 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# Preserve tiddlywki.info
+cp ./*wiki/tiddlywiki.info .
+# change to static
+cp static.info ./*wiki/tiddlywiki.info
+# Build
 tiddlywiki ./*wiki --build
+# Restore info file
+mv tiddlywiki.info ./*wiki
 
 echo "Mirroring files..."
 rsync -azv --delete ./wiki/output/ /Users/eechris/Sites/journal
