2979d41a1a4b62dcdc9b9c29ba0933ee89401efb
Chris Jobling <cpjobling@gmail.com>
2019-07-01 18:21:03 +0100

    windows changes

--- twj.bat
+++ twj.bat
@@ -1,2 +1,2 @@
-cd C:\Users\Chris\Dropbox\TiddlyWiki\Wikis\journal.cpjobling.net
+cd %home%\Dropbox\TiddlyWiki\Wikis\journal.cpjobling.net
 C:\"Program Files"\nodejs\node.exe .\node_modules\tiddlywiki\tiddlywiki.js ./wiki --listen port=9060 path-prefix=/Journal
